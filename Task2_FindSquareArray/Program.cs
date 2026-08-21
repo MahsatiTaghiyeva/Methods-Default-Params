3b4141b5d69d003562bac8c62b029bfeddc1406f
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-21 13:36:26 +0400

    added task 2

---
+++ Task2_FindSquareArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] FindSquareArray(params int[] array)
+{
+    int[] newnums = new int[array.Length];
+    for(int i =0;i<array.Length; i++)
+    {
+        int square = array[i] * array[i];
+        newnums[i] = square;
+    }
+    return newnums;
+}
+foreach(int num in FindSquareArray(2, 3, 4, 5))
+{
+    Console.WriteLine(num);
+}
