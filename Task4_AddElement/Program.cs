11b1202524392ac1216bb3c52e98feff8f438332
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-21 13:59:16 +0400

    added task 4

---
+++ Task4_AddElement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO.Pipelines;
+
+int[] AddElement(int element, params int[] array)
+{
+    int[] newArray = new int[array.Length+1];
+    for(int i = 0;i< array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    newArray[newArray.Length-1] = element;
+    return newArray;
+}
+int[] result = AddElement(64,45,36,91,11);
+foreach( int num in result)
+{
+    Console.WriteLine(num);
+}
