8c9e460e8a0bf4ac13400ebd3a1f48b3a4718ec5
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-21 13:39:42 +0400

    added task3

---
+++ Task3_RemoveSpace/Program.cs
@@ -0,0 +1,13 @@
+﻿string RemoveSpace(string input)
+{
+    string changed =" ";
+    foreach(char letter in input)
+    {
+        if(letter != ' ')
+        { 
+            changed += letter;
+        }
+    }
+    return changed;
+}
+Console.WriteLine(RemoveSpace("He llo World !"));
