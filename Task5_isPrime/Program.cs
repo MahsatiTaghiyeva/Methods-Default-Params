998284b8174f4b6fbd519db8f08a1df81cd84135
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 17:36:16 +0400

    added task 5 murekkeb ve ya sade

---
+++ Task5_isPrime/Program.cs
@@ -0,0 +1,12 @@
+﻿bool isPrime(int a)
+{
+    if (a < 0)
+        return false;
+    for(int i =2; i< a; i++)
+    {
+        if (a % i == 0)
+            return false;
+    }
+    return true;
+}
+Console.WriteLine(isPrime(5));
