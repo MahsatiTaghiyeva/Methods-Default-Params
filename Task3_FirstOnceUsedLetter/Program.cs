90443ce5ecbaeed11ddfa73f3be3a7f662d536b7
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 18:04:03 +0400

    added task 3 first once used letter

---
+++ Task3_FirstOnceUsedLetter/Program.cs
@@ -0,0 +1,20 @@
+﻿char usedOnceLetter(string word){
+foreach(char letter in word){
+    int count = 0;
+    for(int i = 0; i < word.Length; i++)
+        {
+            if(letter == word[i])
+                {
+                count++;
+                }
+        }
+        if(count == 1)
+            {
+                return letter;
+            }
+    }   
+    return '\0';
+}
+
+char result = usedOnceLetter("kertenkele");
+Console.WriteLine(result);
