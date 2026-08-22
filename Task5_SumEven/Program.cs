25a99c6311643fa18e9a49434bf13149e50fc10f
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 17:38:46 +0400

    added task 5 sum of evevn numbers in array

---
+++ Task5_SumEven/Program.cs
@@ -0,0 +1,13 @@
+﻿int SumEven(params int[] array)
+{
+    int sum = 0;
+    foreach (int num in array)
+    {
+        if (num % 2 == 0)
+            sum += num;
+    }
+    {
+        return sum;
+    }
+}
+Console.WriteLine(SumEven(2,5,4,7));
