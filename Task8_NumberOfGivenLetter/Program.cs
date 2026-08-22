7284dbc790f3c90234c84aba0a6a554dd64965a5
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 22:38:06 +0400

    added tsk 8

---
+++ Task8_NumberOfGivenLetter/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] list = {"apple", "banana", "watermelon", "orange"};
+int countOfLettera(string[] list){
+char find = 'a';
+int count = 0;
+foreach(string word in list)
+{
+    for(int i =0; i < word.Length; i++)
+    {
+        if(word[i] == find)
+        {
+            count ++;
+        }
+    }
+}
+return count;
+
+}
+Console.WriteLine(countOfLettera(list));
