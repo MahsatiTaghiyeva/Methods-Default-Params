91c41ae1026654943fc16e4dfe23aa2354a75a78
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 22:16:29 +0400

    added task 6

---
+++ Task6_MostLetterCount/Program.cs
@@ -0,0 +1,43 @@
+﻿int mostLetterCount(string word){
+int max = 0;
+foreach(char letter in word){
+    int count = 0;
+   
+    for(int i = 0; i < word.Length; i++)
+        {
+            if(letter == word[i])
+                {
+                count++;
+                }
+        }
+        if(count > max)
+            {
+                max = count;
+            }
+    }
+    return max;
+}
+char mostCountLetter(string word){
+char l = ' ';
+int max =0;
+foreach(char letter in word){
+    int count = 0;
+   
+    for(int i = 0; i < word.Length; i++)
+        {
+            if(letter == word[i])
+                {
+                count++;
+                }
+        }
+        if(count > max)
+            {
+                max = count;
+                l = letter;                
+            }
+    }
+    return l;
+}
+
+    Console.WriteLine(mostLetterCount("kertenkele"));
+    Console.WriteLine(mostCountLetter("kertenkele"));
