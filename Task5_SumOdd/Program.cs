3dfda4c13e53585f78939e0e1dbbbf4f295c064f
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 17:33:42 +0400

    added task 5 sum of odd numbers

---
+++ Task5_SumOdd/Program.cs
@@ -0,0 +1,13 @@
+﻿int SumOdd(int n, int m)
+{
+    int sum = 0;
+    for (int i = n; i <= m; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum+=i;
+        }
+    }
+    return sum;
+}
+Console.WriteLine(SumOdd(5, 17));
