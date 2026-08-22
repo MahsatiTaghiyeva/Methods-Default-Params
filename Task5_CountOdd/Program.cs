4a65467cfe535cec68494861324a7c3c195ac54e
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 17:29:24 +0400

    added rask cout odd nums

---
+++ Task5_CountOdd/Program.cs
@@ -0,0 +1,13 @@
+﻿int CountOdd(int n, int m)
+{
+    int count = 0;
+    for(int i = n; i<= m; i++)
+    {
+        if (i % 2 != 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine(CountOdd(5,7));
