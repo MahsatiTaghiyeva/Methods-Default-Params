ec38caaab5609cff70b01c7d763d741866c4482f
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 17:54:00 +0400

    added task 2 remove min from array

---
+++ Task2_RemoveMin/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] removeMin(params int[] numbers){
+int[] newnumbers = new int[numbers.Length-1];
+int min = numbers[0];
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i]< min)
+    {
+        min = numbers[i];
+    }
+}
+int index = 0;
+foreach(int number in numbers)
+{
+    
+    if(number == min)
+    {
+        continue;
+    }
+    newnumbers[index] = number;
+    index++;
+    
+}
+return newnumbers;
+}
+
+int[] result =  removeMin(7, 12, 5, 8);
+foreach(int number in result)
+{
+    Console.WriteLine(number);
+}
