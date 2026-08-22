180e85c3b3d22ebabe941a211dfb6594b6649374
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 22:23:52 +0400

    added task 7

---
+++ Task7_RemoveSpaces/Program.cs
@@ -0,0 +1,12 @@
+﻿string removeSpaces(string sentence){
+string newSentence = "";
+foreach (char letter in sentence)
+{
+    if(letter != ' ')
+    {
+        newSentence += letter;
+    }
+}
+    return newSentence;
+}
+Console.WriteLine(removeSpaces("Hello World !"));
