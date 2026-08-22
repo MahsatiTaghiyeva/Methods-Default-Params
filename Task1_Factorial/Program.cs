886c47139d8f6698a7a24e9465350abacbdc7d04
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-22 17:46:58 +0400

    added task 1 factorial

---
+++ Task1_Factorial/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] factorialArray(params int[] numbers){
+for( int i = 0; i < numbers.Length; i++)
+{
+    int factorial = 1;
+    for(int j = 1; j <= numbers[i]; j++)
+    {
+        factorial *= j;
+    }
+    numbers[i] = factorial;
+}
+return numbers;
+}
+int[] result = factorialArray(5,4,6);
+foreach(int number in result)
+{
+    Console.WriteLine(number);
+}
