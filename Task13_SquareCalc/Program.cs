73f3e2e2eff3803d71e48ed064f98783f5673b39
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-24 12:14:44 +0400

    added task 13

---
+++ Task13_SquareCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿int squareEvenPositive(int n){
+while(n%2!=0 || n < 0)
+{
+    Console.WriteLine("Musbet ve cut reqem daxil et: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+n = n*n;
+return n;
+}
+Console.WriteLine("Musbet ve cut reqem daxil et: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(squareEvenPositive(num));
