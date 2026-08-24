a04594bbba8c5dd27ca23405064eb14f299bd3a3
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-24 12:03:19 +0400

    added task 11

---
+++ Task11_ExistsA/Program.cs
@@ -0,0 +1,16 @@
+﻿bool existsA(string word){
+bool exists = false;
+foreach(char letter in word)
+{
+        if(letter == 'A')
+        {
+            exists = true;
+            break;
+        } 
+}
+if (exists){
+    return true;
+}else
+    return false;
+}
+Console.WriteLine(existsA("Apple"));
