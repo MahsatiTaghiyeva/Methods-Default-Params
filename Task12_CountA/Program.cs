c04dbe4deeb408500dcc4eba781b51e82fb06ce5
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-24 12:08:16 +0400

    added task 12

---
+++ Task12_CountA/Program.cs
@@ -0,0 +1,12 @@
+﻿int countA(string word){
+int count = 0;
+foreach(char letter in word)
+{
+        if(letter == 'A')
+        {
+            count++;;
+        }
+}
+return count;
+}
+Console.WriteLine(countA("AppleA"));
